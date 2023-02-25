f078f2290e0f5125e0f5697af4054ef54ded4935
cgwatson193 <78247585+cgwatson193@users.noreply.github.com>
2023-02-25 17:24:54 -0600

    skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,14 +1,10 @@
 import React from 'react'
-import HTML from '../assets/html.png';
-import CSS from '../assets/css.png';
-import JavaScript from '../assets/javascript.png';
-
-import Java from '../assets/java.png';
 import Python from '../assets/python.png';
-import CPlus from '../assets/cplus.png';
+import Typescript from '../assets/ts.png';
+import Docker from '../assets/docker.png';
+import Next from '../assets/next-js.svg';
 import Github from '../assets/github.png';
-import Aws from '../assets/aws.png';
-import R from '../assets/r.png';
+import Neovim from '../assets/neovim.svg';
 const Skills = () => {
   return (
     <div name='skills' className='w-full h-screen bg-[#0a192f] text-gray-300'>
@@ -20,31 +16,30 @@ const Skills = () => {
             </div>
             <div className='w-full grid grid-cols-2 sm:grid-cols-3 gap-6 text-center py-8'>
             <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <img className='w-20 mx-auto' src={Typescript} alt="PYTHON icon"></img>
+                    <p className='my-4'>TYPESCRIPT</p>
+                </div>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <img className='w-20 mx-auto' src={Next} alt="JAVA icon"></img>
+                    <p className='my-4'>NEXT.JS</p>
+                </div>
+            <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
                     <img className='w-20 mx-auto' src={Python} alt="PYTHON icon"></img>
                     <p className='my-4'>PYTHON</p>
                 </div>
+
                 <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                    <img className='w-20 mx-auto' src={CPlus} alt="C++ icon"></img>
-                    <p className='my-4'>C++</p>
+                    <img className='w-20 mx-auto' src={Neovim} alt="JAVA icon"></img>
+                    <p className='my-4'>NEOVIM</p>
                 </div>
                 <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
                     <img className='w-20 mx-auto' src={Github} alt="Github icon"></img>
                     <p className='my-4'>GIT</p>
                 </div>
-                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                    <img className='w-20 mx-auto' src={Java} alt="JAVA icon"></img>
-                    <p className='my-4'>JAVA</p>
-                </div>
-                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                    <img className='w-20 mx-auto' src={Aws} alt="AWS icon"></img>
-                    <p className='my-4'>AWS</p>
-                    </div>
-              
-             
-             
-                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                    <img className='w-20 mx-auto' src={JavaScript} alt="JAVASCRIPT icon"></img>
-                    <p className='my-4'>JAVASCRIPT</p>
+
+            <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <img className='w-20 mx-auto' src={Docker} alt="PYTHON icon"></img>
+                    <p className='my-4'>DOCKER</p>
                 </div>
                
                 
