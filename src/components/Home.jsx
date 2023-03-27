48181800f8501d81d21224ef5ac9e5943126948d
cgwatson193 <78247585+cgwatson193@users.noreply.github.com>
2023-03-27 15:05:49 -0500

    wording

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
             <p className='text-[#5ce1e6]'>Hi, my name is</p>
             <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>Cullen Watson</h1>
             <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>I'm a Full Stack Developer.</h2>
-            <p className='text-[#8892b0] py-4 max-w-[700px]'>I'm a Junior at Southern Methodist University studying Computer Science, Statistics, and Finance, with a plan to specialize in Machine Learning. My goal is to obtain a Master’s in Computer Science once I graduate from SMU. Currently, I am seeking a Summer 2023 Internship in Software Engineering.
+            <p className='text-[#8892b0] py-4 max-w-[700px]'>I'm a Junior at Southern Methodist University studying Computer Science, Statistics, and Finance, with a plan to specialize in Machine Learning. My goal is to obtain a Master’s in Computer Science once I graduate from SMU. Currently, I am seeking a Fall 2023 Internship in Software Engineering.
             </p>
             <div>
               <Link  to="about" smooth={true} duration={500}>
